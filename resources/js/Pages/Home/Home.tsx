44934e1e4374cb7386353fef9030e36334de79ba
yohanesrioirsan <rioirsan8@gmail.com>
2025-01-01 21:20:53 +0700

    update: set title on everypage

--- resources/js/Pages/Home/Home.tsx
+++ resources/js/Pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Layout from "@/Layouts/Layout";
 import Hero from "@/Container/Hero";
+import { Head } from "@inertiajs/react";
 
 export default function Home(){
     return(
         <Layout>
+            <Head title="Anonin - Kirim pesan misterius secara anonim" />
             <Hero />
         </Layout>
     )
--- resources/js/Pages/SendMessage/SendMessagePage.tsx
+++ resources/js/Pages/SendMessage/SendMessagePage.tsx
@@ -1,9 +1,11 @@
 import Layout from "@/Layouts/Layout";
 import SendMessage from "@/Container/SendMessage";
+import { Head } from "@inertiajs/react";
 
 export default function SendMessagePage() {
     return (
         <Layout>
+            <Head title="Anonin - Kirim Pesan" />
             <SendMessage />
         </Layout>
     );
