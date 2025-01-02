7344339f901da1345c7f0270e42f069866629080
yohanesrioirsan <rioirsan8@gmail.com>
2025-01-02 20:15:00 +0700

    update: added meta & favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Website seru buat kirim-kiriman pesan anonim! Tempat yang pas buat curhat, bercanda, atau kasih kejutan rahasia. Cari tahu apakah ada pesan anonim buat kamu di sini!">
+    <meta name="keywords"
+        content="pesan anonim, kirim pesan rahasia, website anonim, curhat online, kejutan rahasia, pesan misterius, cari pesan anonim, seru, rahasia, lucu, penasaran">
+    <meta name="author" content="Yohanes Rio Irsan">
+    <meta name="robots" content="index, follow">
 
     <title inertia>{{ config('app.name') }}</title>
 
