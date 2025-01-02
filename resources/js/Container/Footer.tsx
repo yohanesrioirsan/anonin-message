4140a0efef426f119baa63fc94287bb62346f835
yohanesrioirsan <rioirsan8@gmail.com>
2025-01-02 23:19:50 +0700

    update: set animated heart emoji

--- resources/js/Container/Footer.tsx
+++ resources/js/Container/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="container mx-auto pb-6">
             <p className="text-center text-sm text-black">
-                @2025 - Anon<span className="text-blue-500">in</span> - Made with ❤️ by Yohanes Rio Irsan
+                @2025 - Anon<span className="text-blue-500">in</span> - Made with <span className="inline-block animate-heartbeat">❤️</span> by Yohanes Rio Irsan
             </p>
         </footer>
     );
